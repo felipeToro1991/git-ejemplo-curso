f4f121f0ec2aba06cc98a1b803cfc8912e6f6527
felipeToro1991 <felipetoro1991@gmail.com>
2021-04-01 18:36:19 -0300

    Actualizacion page cotizacion

--- src/test/java/Page/Cotizacion.java
+++ src/test/java/Page/Cotizacion.java
@@ -1,5 +1,11 @@
 package Page;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class Cotizacion {
+
+    @FindBy(xpath = "")
+    private WebElement ejemplo;
 
 }
